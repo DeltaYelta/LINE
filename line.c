ee3bf6d3d8540406bbc88e80501cb022b28e5fe8
DeltaYelta <22403779+DeltaYelta@users.noreply.github.com>
2022-03-16 14:43:02 +0000

    upload through github since vs isn't

--- line.c
+++ line.c
@@ -1,21 +1,55 @@
 /* LINE Is Not Ed
  * Warning, far from complete as this stage.
  * A small text editor, designed for embedded and recovery systems.
- * Also designed to be extremely portable, written in pure C90 and only the standard library.
- * Highly suggested to compile yourself, for row and column configuration. */
+ * Also designed to be extremely portable, written in pure ANSI C and only with the standard library.
+ * Highly suggested to compile yourself, for row and column configuration.
+ */
 
 #include <stdio.h>
 #include <string.h>
 
-/* Avoid use of malloc and free and stuff*/
+ /* Avoid use of malloc and free and stuff */
 #define malloc(void) return 0
-/* We assume the terminal is 48 rows CHANGE THIS */
-#define TERM_ROWS (48 - 3)
+#define free(void) return 0
+#define realloc(void) return 0
+/* We assume the terminal is 30 rows CHANGE THIS */
+#define TERM_ROWS (30 - 3)
 /* Also assume the terminal has 60 columns CHANGE THIS */
 #define TERM_COLUMNS 60
 /* Maximum buffer size */
 #define BUF_SIZE (TERM_ROWS * TERM_COLUMNS)
 
+/* Returns length of file, side effect of creating the file if it doesn't exist */
+long filelen(char* filename) {
+	FILE* temp = fopen(filename, "a");
+	long ret = ftell(temp);
+	fclose(temp);
+	return ret;
+}
+
+/* Read entire file into a 2 dimensional char array, (ab)uses filelen()'s side effect */
+char** readfile(char* filename) {
+	char** ret;
+	long len = filelen(filename);
+	FILE* tfile = fopen(filename, "r");
+	char chr;
+	int line = 0;
+	for (long i = 0; i < len; i++) {
+		chr = fgetc(tfile);
+		if (feof(tfile) != 0) {
+			break;
+		}
+		else if (chr == '\n') {
+			line++;
+			i = 0;
+		}
+		else {
+			ret[line][i] = chr;
+			i++;
+		}
+	}
+	return ret;
+}
 
 /* Center text, padding with octothorpes (slightly off but who cares) */
 char* hashpad(char* text, int columns) {
@@ -33,14 +67,17 @@ char* hashpad(char* text, int columns) {
 }
 
 /* Reprints the screen (best portable method I could come up with) */
-void setup(char* text, char* filename, int rows, int columns) {
+void reprint(char* text, char* filename, int rows, int columns) {
 	printf("%s\n", hashpad(filename, columns));
+	printf(text);
+
 };
 
-int main(int argc, char **argv) {
-	FILE* openfile = fopen("./e.txt", "a+");
-	fputs("hello", openfile);
-	setup("hi", "./e.txt", TERM_ROWS, TERM_COLUMNS);
+int main(int argc, char** argv) {
+	FILE* file = fopen("e.txt", "a");
+	fputs("hello", file);
+	printf("%d", ftell(file));
+	/*reprint("hi", "./e.txt", TERM_ROWS, TERM_COLUMNS);*/
 	printf("hi");
 	exit(3);
 }
